290ea93166bc50b78ca12dafae208e34c56d72fd
bjornsgithub <bjornsmail@icloud.com>
2021-01-16 10:37:23 +0100

    feat: :art: Init

---
+++ projects/test-lib/src/lib/test-lib.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { TestControlComponent } from './test-control/test-control.component';
+
+@NgModule({
+  declarations: [TestControlComponent],
+  imports: [
+  ],
+  exports: []
+})
+export class TestLibModule { }
---
+++ projects/test-lib/src/public-api.ts
@@ -0,0 +1,8 @@
+/*
+ * Public API Surface of test-lib
+ */
+
+export * from './lib/test-lib.service';
+export * from './lib/test-lib.module';
+export * from './lib/test-control/test-control.component';
+
